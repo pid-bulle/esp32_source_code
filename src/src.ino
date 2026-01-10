26b992372ca2d7299b0d17312cfd15980ad0db8d
argom01 <nor.siczek@tutanota.com>
2026-01-10 18:02:07 +0100

    bling bling bling sahur

--- src/src.ino
+++ src/src.ino
@@ -1,11 +1,21 @@
+const int LED_PIN = 15; // GPIO 15
+bool ledState = LOW;
+unsigned long previousMillis = 0;
+const unsigned long interval = 500; // toggle every 500 ms -> 1 Hz blink
+
 void setup() {
   Serial.begin(115200);
-  Serial.println("ESP32 ready");
+  Serial.println("ESP32 ready — blinking on GPIO15");
+
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, ledState);
 }
 
 void loop() {
-  delay(1000);
-    Serial.println("ESP32 ready");
-
-    
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    ledState = !ledState;
+    digitalWrite(LED_PIN, ledState);
+  }
 }
